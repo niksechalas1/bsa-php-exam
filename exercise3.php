9ccd963b5eda99d27412ddbe5e8902ab5edf044d
Nichole Echalas <97090817+niksechalas1@users.noreply.github.com>
2023-01-31 16:02:34 +0800

    Update exercise3.php

--- exercise3.php
+++ exercise3.php
@@ -1,1 +1,50 @@
 
+<?php
+  function getTotal($arr)
+  {
+    $total = 0;
+
+    foreach ($arr as $num) {
+      if (isDigitPresent($num, 8)) {
+        $total += 5;
+      } else {
+        if (isNumEven($num)) {
+          $total += 1;
+        } else {
+          $total += 3;
+        }
+      }
+    }
+
+    echo("Total: " . $total . "\r\n");
+  }
+
+  function isNumEven($number){
+    if($number == 0)
+        return 1;
+    else if($number == 1)
+        return 0;
+    else if($number<0)
+        return isNumEven(-$number);
+    else
+        return isNumEven($number-2);        
+  }
+
+  function isDigitPresent($x, $d)
+  {
+    while ($x > 0)
+    {
+        if ($x % 10 == $d)
+            break;
+
+        $x = $x / 10;
+    }
+    return ($x > 0);
+  }
+
+  $arr1 = [1,2,3,4,5,];
+  $arr2 = [8,5,2];
+
+  getTotal($arr1);
+  getTotal($arr2);
+?>
